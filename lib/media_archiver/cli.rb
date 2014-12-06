79608977d00b6f2e7d855df2f247630e0089d263
David Ramalho <dramalho@gmail.com>
2014-12-06 19:09:32 +0000

    WIP: Copy method

--- lib/media_archiver/cli.rb
+++ lib/media_archiver/cli.rb
@@ -15,18 +15,41 @@ module MediaArchiver
 
       path = File.expand_path(path)
 
-      MediaFileUtils.new(path).each(options[:recursive]) do |file|
-        puts output_path(file, options[:output_dir], options[:output_template])
+      MediaFileUtils.new(path).each(config[:recursive]) do |file|
+        puts output_path(file, config[:output_dir], config[:output_template])
       end
     end
 
     desc 'copy [DIR] [OUTPUT_DIR]', 'Scans a folder and archives media files'
-    def copy
+    option :output_dir, aliases: :o, default: Dir.pwd
+    option :recursive, aliases: :r, type: :boolean, default: true
+    option :output_template, default: ':date_created/:camera_maker/:camera_model'
+    option :configuration_file, aliases: :c
+    def copy(path = Dir.pwd)
+      config = configurations(options)
+
+      path = File.expand_path(path)
+
+      MediaFileUtils.new(path).each(config[:recursive]) do |file|
+        dest = output_path(file, config[:output_dir], config[:output_template])
+        FileUtils.mkdir_p(File.dirname(dest))
+
+        output = ["File: #{dest}"]
+        output << if File.exist?(dest)
+                    '[SKIP]'
+                  else
+                    FileUtils.cp(file.path, dest)
+                    '[OK]'
+                  end
+
+        puts output.join(' ')
+      end
     end
 
     private
 
     def configurations(options)
+      options = options.each_with_object({}) { |(k,v),acc| acc[k.to_sym] = v }
       system_configurations.merge(options)
     end
 
@@ -44,7 +67,7 @@ module MediaArchiver
     end
 
     def load_config_file(path)
-      YAML.load_file path
+      YAML.load_file(path)
     end
 
     def output_path(file, output_path, template)
